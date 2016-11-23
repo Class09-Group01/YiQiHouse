9b668b8077e50533ebeb8c06d41d84dbd27fdedf
zjb-bo <779815091@qq.com>
2016-11-23 19:53:19 +0800

    首页，底部跳转fragment完成

---
+++ app/src/main/java/com/bwf/aiyiqi/gui/fragment/MessageFragment.java
@@ -0,0 +1,14 @@
+package com.bwf.aiyiqi.gui.fragment;
+
+import com.bwf.aiyiqi.R;
+
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+
+public class MessageFragment extends BaseFragment {
+    @Override
+    protected int getContentViewResId() {
+        return R.layout.fragment_message;
+    }
+}
---
+++ app/src/main/java/com/bwf/aiyiqi/gui/fragment/MyFragment.java
@@ -0,0 +1,14 @@
+package com.bwf.aiyiqi.gui.fragment;
+
+import com.bwf.aiyiqi.R;
+
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+
+public class MyFragment extends BaseFragment {
+    @Override
+    protected int getContentViewResId() {
+        return R.layout.fragment_my;
+    }
+}

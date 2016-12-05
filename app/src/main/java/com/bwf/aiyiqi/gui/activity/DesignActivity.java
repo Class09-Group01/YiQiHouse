e552414dfd7ee23e5008f1a756e7d0579e8f9678
lingchen52 <lingchen52@foxmail.com>
2016-12-05 09:17:14 +0800

    html

---
+++ app/src/main/java/com/bwf/aiyiqi/gui/activity/DesignActivity.java
@@ -0,0 +1,24 @@
+package com.bwf.aiyiqi.gui.activity;
+
+import com.bwf.aiyiqi.R;
+
+/**
+ * Created by lingchen52 on 2016/12/3.
+ */
+
+public class DesignActivity extends BaseActivity {
+    @Override
+    protected void initDatas() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected int getContentViewResId() {
+        return R.layout.activity_design;
+    }
+}
